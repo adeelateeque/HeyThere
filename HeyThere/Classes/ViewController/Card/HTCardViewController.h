5c513539c7ed23c3240d3e039d9b517c1d7c828a
Adeel <adeelateeque@gmail.com>
2014-01-15 16:05:22 +0800

    Doing views

---
+++ HeyThere/Classes/ViewController/Card/HTCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTCardViewController.h
+//  HeyThere
+//
+//  Created by Adeel on 15/1/14.
+//  Copyright (c) 2014 HeyThere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTCardViewController : UIViewController
+
+@end
---
+++ HeyThere/Classes/ViewController/Card/HTCardViewController.m
@@ -0,0 +1,38 @@
+//
+//  HTCardViewController.m
+//  HeyThere
+//
+//  Created by Adeel on 15/1/14.
+//  Copyright (c) 2014 HeyThere. All rights reserved.
+//
+
+#import "HTCardViewController.h"
+
+@interface HTCardViewController ()
+
+@end
+
+@implementation HTCardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
