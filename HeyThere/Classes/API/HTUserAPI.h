6e674176d93c57253d22e400a59ccd969c3f1678
Adeel <adeelateeque@gmail.com>
2014-01-15 15:35:30 +0800

    Setup cocoapods

---
+++ HeyThere/Classes/API/HTUserAPI.h
@@ -0,0 +1,10 @@
+//
+// Created by Adeel on 15/1/14.
+// Copyright (c) 2014 HeyThere. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HTUserAPI : NSObject
+@end
---
+++ HeyThere/Classes/API/HTUserAPI.m
@@ -0,0 +1,12 @@
+//
+// Created by Adeel on 15/1/14.
+// Copyright (c) 2014 HeyThere. All rights reserved.
+//
+
+#import "HTUserAPI.h"
+
+
+@implementation HTUserAPI {
+
+}
+@end
